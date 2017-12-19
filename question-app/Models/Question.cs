eb02701c59d40ec4077aa98ad1c1e5be847b043b
ConiX21 <nicolas.gasquet@nerys.biz>
2017-12-19 04:05:02 +0100

    Add all models

---
+++ question-app/Models/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace question_app.Models
+{
+    public partial class Question
+    {
+        public Question()
+        {
+            Answer = new HashSet<Answer>();
+            UserAnswer = new HashSet<UserAnswer>();
+        }
+
+        public int IdQuestions { get; set; }
+        public string QuestionText { get; set; }
+        public int QuestionnaireIdQuestionnaire { get; set; }
+
+        public Questionnaire QuestionnaireIdQuestionnaireNavigation { get; set; }
+        public ICollection<Answer> Answer { get; set; }
+        public ICollection<UserAnswer> UserAnswer { get; set; }
+    }
+}
---
+++ question-app/Models/Questionnaire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace question_app.Models
+{
+    public partial class Questionnaire
+    {
+        public Questionnaire()
+        {
+            Question = new HashSet<Question>();
+            UserAnswer = new HashSet<UserAnswer>();
+        }
+
+        public int IdQuestionnaire { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Question> Question { get; set; }
+        public ICollection<UserAnswer> UserAnswer { get; set; }
+    }
+}
